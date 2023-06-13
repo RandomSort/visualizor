f8550804c2a7dc6b7deb63cb92db01c9324e2bf8
Johan Sigfred Abildskov <randomsort@gmail.com>
2023-06-13 06:19:10 +0000

    Success with react-p5-wrapper

--- first-steps/src/App.js
+++ first-steps/src/App.js
@@ -1,18 +1,59 @@
-import React from 'react'
-import Sketch from 'react-p5'
+import * as React from "react";
+import { ReactP5Wrapper } from "@p5-wrapper/react";
 
-function App() {
-  const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(500, 400).parent(canvasParentRef)
-  }
-  
-  const draw = p5 => {
-    p5.background(255, 130, 20)
-    p5.ellipse(100, 100, 100)
-    p5.ellipse(300, 100, 100)
-  }
-  
-  return <Sketch setup={setup} draw={draw} />
+function sketch(p5) {
+  let size = 1;
+  let circles = []
+
+
+  p5.setup = () => p5.createCanvas(600, 400);
+
+
+  p5.updateWithProps = props => {
+    if (props.size) {
+      size = props.size;
+    }
+    if (props.circles) {
+      circles = props.circles;
+    }
+  };
+
+  p5.draw = () => {
+    if (circles) {
+      circles.forEach((circle, index) => {
+        p5.circle(circle.x, circle.y, size)
+      })
+    }
+    
+    [{x: 500, y:300}].forEach((circle, index) => {
+      p5.circle(circle.x, circle.y, size)
+    })
+    p5.circle(300,200, size)
+  };
+
 }
 
-export default App
+export default function App() {
+  const [state, setState] = React.useState({
+    size: 10,
+    circles: [{x: 50, y:50}],
+  });
+
+  const increaseSize = React.useCallback(() => {
+    setState(state => ({ ...state, size: state.size + 1 }));
+  }, []);
+
+  const addCircle = React.useCallback(() => {
+    let newCircles = state.circles;
+    newCircles.push({x: 10, y: 10});
+    setState(state => ({ ...state, circles: newCircles }));
+  }, [state.circles]);
+
+  return <React.Fragment>
+     <ReactP5Wrapper sketch={sketch} size={state.size} circles={state.circles} /> 
+      <button onClick={increaseSize}>Inc</button>
+      <button onClick={addCircle}>Add</button>
+
+    </React.Fragment>
+  
+}
