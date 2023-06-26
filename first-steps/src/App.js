3776a79fd9e1c191c046e938e16c161e0b56d36a
Johan Sigfred Abildskov <randomsort@gmail.com>
2023-06-26 19:33:49 +0000

    sending messages

--- first-steps/src/App.js
+++ first-steps/src/App.js
@@ -4,31 +4,39 @@ import { ReactP5Wrapper } from "@p5-wrapper/react";
 function sketch(p5) {
   let size = 1;
   let circles = []
+  let asdf = []
 
 
   p5.setup = () => p5.createCanvas(600, 400);
 
 
   p5.updateWithProps = props => {
+    console.log("updateProps")
     if (props.size) {
       size = props.size;
     }
     if (props.circles) {
       circles = props.circles;
     }
+    if (props.asdf) {
+      console.log(props.asdf)
+      asdf = props.asdf
+    }
   };
 
   p5.draw = () => {
+    p5.background(255)
     if (circles) {
       circles.forEach((circle, index) => {
         p5.circle(circle.x, circle.y, size)
       })
     }
     
-    [{x: 500, y:300}].forEach((circle, index) => {
-      p5.circle(circle.x, circle.y, size)
-    })
-    p5.circle(300,200, size)
+    if (asdf) {
+      asdf.forEach((asd, index) => {
+        p5.rect(asd.x, asd.y, size, size)
+      })
+    }
   };
 
 }
@@ -38,6 +46,7 @@ export default function App() {
   const [state, setState] = React.useState({
     size: 10,
     circles: [{x: 50, y:50}],
+    asdf: [{"name": "A", x: 200, y:200}, {"name": "B", x: 400, y: 200}]
   });
 
   const increaseSize = React.useCallback(() => {
@@ -49,25 +58,32 @@ export default function App() {
     let frameId
     const frame = time => {
       setFrameTime(time)
+      let newCircles = []
+      state.circles.forEach((elm) => {
+        if(elm.x <= state.asdf[1].x) {
+          newCircles.push({x: elm.x +1, y: elm.y });
+        }
+      })
+      setState(state => ({ ...state, circles: newCircles }))
       frameId = requestAnimationFrame(frame)
     }
 
     requestAnimationFrame(frame)
     return () => cancelAnimationFrame(frameId)
 
-  },[])
+  },[state.circles, state.asdf])
 
   const addCircle = React.useCallback(() => {
     let newCircles = state.circles;
-    newCircles.push({x: 10, y: 10});
+    newCircles.push({x: state.asdf[0].x, y: state.asdf[0].y});
     setState(state => ({ ...state, circles: newCircles }));
-  }, [state.circles]);
+  }, [state.circles, state.asdf]);
 
   return <React.Fragment>
-     <ReactP5Wrapper sketch={sketch} size={state.size} circles={state.circles} /> 
+     <ReactP5Wrapper sketch={sketch} size={state.size} circles={state.circles} asdf={state.asdf} /> 
       <button onClick={increaseSize}>Inc</button>
       <button onClick={addCircle}>Add</button>
-      {frameTime}
+      {state.circles.length}
     </React.Fragment>
   
 }
